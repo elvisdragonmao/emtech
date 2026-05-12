931e772775a6636f79ee14c26b7835a01d0b7d4a
Elvis Dragon Mao <info@elvismao.com>
2026-05-13 00:50:49 +0800

    fix: use passthrough image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import { rehypeCallouts } from "./src/plugins/rehype-callouts.js";
 import { rehypeCodeBlocks } from "./src/plugins/rehype-code-blocks.js";
 import { rehypeImageCaptions } from "./src/plugins/rehype-image-captions.js";
 
 export default defineConfig({
+	image: {
+		service: passthroughImageService()
+	},
 	markdown: {
 		rehypePlugins: [rehypeCallouts, rehypeImageCaptions, rehypeCodeBlocks]
 	}
