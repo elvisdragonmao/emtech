4682bb16a46aa4f3600af82f7d8337aa41e2f524
Elvis Dragon Mao <info@elvismao.com>
2026-05-13 00:50:37 +0800

    feat: add code block copy controls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import { rehypeCallouts } from "./src/plugins/rehype-callouts.js";
+import { rehypeCodeBlocks } from "./src/plugins/rehype-code-blocks.js";
 import { rehypeImageCaptions } from "./src/plugins/rehype-image-captions.js";
 
 export default defineConfig({
 	markdown: {
-		rehypePlugins: [rehypeCallouts, rehypeImageCaptions]
+		rehypePlugins: [rehypeCallouts, rehypeImageCaptions, rehypeCodeBlocks]
 	}
 });
