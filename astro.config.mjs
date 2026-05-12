eea7baf55a6aeb9a881dfb0e024175d402268daa
Elvis Dragon Mao <info@elvismao.com>
2026-05-12 22:48:39 +0800

    feat: image captions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import { rehypeCallouts } from "./src/plugins/rehype-callouts.js";
+import { rehypeImageCaptions } from "./src/plugins/rehype-image-captions.js";
 
 export default defineConfig({
 	markdown: {
-		rehypePlugins: [rehypeCallouts]
+		rehypePlugins: [rehypeCallouts, rehypeImageCaptions]
 	}
 });
